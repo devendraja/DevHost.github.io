0bc59a8de929542897c505c8382274dba199e0a6
devendraja <devendrajain07@gmail.com>
2018-01-13 11:39:02 +0530

    first website commint

---
+++ js/theme.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    dots:true,
+    nav:false,
+  });
+  
+
+
+
+});
